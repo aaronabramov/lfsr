083d7b6f408613c7edd74107004fb707775ef523
Dmitrii Abramov <dabramov@yahoo-inc.com>
2014-08-19 23:36:41 -0700

    increase timeout to 160s

--- test/statistical.js
+++ test/statistical.js
@@ -8,7 +8,7 @@ context('statistical', function() {
     utils.everyLength(function(n) {
         (function(n) {
             it('max length for n = ' + n + ' is ' + (Math.pow(2, n) - 1), function() {
-                this.timeout(60000);
+                this.timeout(160000);
 
                 var lfsr = new LFSR(n),
                     count = 0,
